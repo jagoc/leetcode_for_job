2b351291d402ade3f902dcf4bbeaa04dd5b96849
jagoc <lee9309@163.com>
2022-12-12 21:13:57 +0800

    array day 3: problem 4 (best solution)

--- array/4_findMedianSortedArrays.cpp
+++ array/4_findMedianSortedArrays.cpp
@@ -20,44 +20,83 @@
 
 using namespace std;
 
+// class Solution {
+// public:
+//     // case: [],[1]; [],[1,2]; [1,3],[2]; [1,2],[3,4];
+//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+//         double result;
+//         int m = nums1.size(), n = nums2.size(), len = m + n;
+//         int i = 0, j = 0;
+//         while (i < m && j < n && i + j < (len - 1) / 2) {
+//             if (nums1[i] < nums2[j])
+//                 ++i;
+//             else
+//                 ++j;
+//         }
+
+//         if (i == m) {
+//             j = (len - 1) / 2 - i;
+//             result = (len & 0x01) ? nums2[j] : (double)(nums2[j] + nums2[j+1]) / 2;
+//         } else if (j == n) {
+//             i = (len - 1) / 2 - j;
+//             result = (len & 0x01) ? nums1[i] : (double)(nums1[i] + nums1[i+1]) / 2;
+//         } else {
+//             if (len & 0x01)
+//                 result = min(nums1[i], nums2[j]);
+//             else {
+//                 if (nums1[i] < nums2[j])
+//                     result = nums1[i] + (i + 1 < m && nums1[i+1] < nums2[j] ? nums1[i+1] : nums2[j]);
+//                 else
+//                     result = nums2[j] + (j + 1 < n && nums2[j+1] < nums1[i] ? nums2[j+1] : nums1[i]);
+//                 result /= 2;
+//             }
+//         }
+//         return result;
+//     }
+// };
+
 class Solution {
 public:
-    // [],[1]; [],[1,2]; [1,3],[2]; [1,2],[3,4];
+    // case: [],[1]; [],[1,2]; [1,3],[2]; [1,2],[3,4];
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        double result;
-        int m = nums1.size(), n = nums2.size(), len = m + n;
-        int i = 0, j = 0;
-        while (i < m && j < n && i + j < (len - 1) / 2) {
-            if (nums1[i] < nums2[j])
-                ++i;
-            else
-                ++j;
-        }
+        int m = nums1.size(), n = nums2.size();
+        if ((m + n & 0x01) != 0)
+            return getKElement(nums1, nums2, (m + n) / 2 + 1);
+        else
+            return (double)(getKElement(nums1, nums2, (m + n) / 2) + getKElement(nums1, nums2, (m + n) / 2 + 1)) / 2;
+    }
 
-        if (i == m) {
-            j = (len - 1) / 2 - i;
-            result = (len & 0x01) ? nums2[j] : (double)(nums2[j] + nums2[j+1]) / 2;
-        } else if (j == n) {
-            i = (len - 1) / 2 - j;
-            result = (len & 0x01) ? nums1[i] : (double)(nums1[i] + nums1[i+1]) / 2;
-        } else {
-            if (len & 0x01)
-                result = min(nums1[i], nums2[j]);
-            else {
-                if (nums1[i] < nums2[j])
-                    result = nums1[i] + (i + 1 < m && nums1[i+1] < nums2[j] ? nums1[i+1] : nums2[j]);
-                else
-                    result = nums2[j] + (j + 1 < n && nums2[j+1] < nums1[i] ? nums2[j+1] : nums1[i]);
-                result /= 2;
+private:
+    int getKElement(vector<int>& nums1, vector<int>& nums2, int k) {
+        int m = nums1.size(), n = nums2.size();
+        int i = 0, j = 0;
+
+        while (i < m && j < n && k > 1) {
+            // 注意偏移量计算
+            int ii = min(i + k / 2 - 1, m - 1);
+            int jj = min(j + k / 2 - 1, n - 1);
+
+            if (nums1[ii] <= nums2[jj]) {
+                k -= ii - i + 1;
+                i = ii + 1;
+            } else {
+                k -= jj - j + 1;
+                j = jj + 1;
             }
         }
-        return result;
+
+        if (i == m)
+            return nums2[j + k - 1];
+        if (j == n)
+            return nums1[i + k - 1];
+
+        return min(nums1[i], nums2[j]);
     }
 };
 
 int main() {
     vector<int> nums1 = {1, 2};
-    vector<int> nums2 = {-1, 3};
+    vector<int> nums2 = {3, 4};
     Solution solution;
     cout << solution.findMedianSortedArrays(nums1, nums2) << endl;
 }
